cdac45853106b61c46b605895b41c4b1c203739f
chai3a <115355077+chai3a@users.noreply.github.com>
2022-11-28 16:53:40 +0530

    Create dsa.cpp

---
+++ dsa.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+struct cards{
+    int face = 1;
+    int shape=3;
+    int color= 2;
+};
+int main ()
+{
+    struct cards c;
+    struct cards c.face
+}
